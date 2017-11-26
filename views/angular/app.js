a0e421db5ffc4217258d016bf7c76a63d1f1299f
Mohit Gawande <mohitgawande@Mohits-MacBook-Air.local>
2017-11-26 17:04:01 +0530

    web api url protocol changed to https

--- views/angular/app.js
+++ views/angular/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('myapp',['ngMaterial','ui.router','ngStorage']);
 
 app.constant('app_settings', {
-    API_URL : 'http://' + location.host + '/api/',
+    API_URL : 'https://' + location.host + '/api/',
     URL_HOST : location.host
 });
